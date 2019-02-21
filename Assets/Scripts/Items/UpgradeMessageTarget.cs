671798f15aab760fb8edbe2f8eff469726583964
chenhuitudou <chenhuitudou@gmail.com>
2019-02-21 16:54:11 +0800

    [modify] 删掉了无用的东西

--- Assets/Scripts/Items/UpgradeMessageTarget.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine.EventSystems;
-public interface UpgradeMessageTarget : IEventSystemHandler {
-    // functions that can be called via the messaging system
-    void OnUpgradeMessage (int changeLv);
-    void OnHpChangeMessage (int changeHP);
-}
